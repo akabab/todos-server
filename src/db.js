0d6bdf4cb1cc64e78c99e7bb5a94dc2975d6bb66
Yoann Cribier <yoann_cribier@hotmail.com>
2018-05-23 17:44:42 +0200

    db - upgraded DATABASE_URL regex parsing to better match different cases (host:port, no password) urls

--- src/db.js
+++ src/db.js
@@ -1,7 +1,7 @@
 const mysql = require('mysql2/promise')
 
 const url = process.env.DATABASE_URL
-const groups = url.match(/mysql:\/\/(\w+):(\w+)@([\w-.]+)\/(\w+)?/)
+const groups = url.match(/mysql:\/\/(\w+):?(\w+)?@([\w.:-]+)\/(\w+)/)
 const options = {
   host: groups[3],
   user: groups[1],
