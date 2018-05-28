bf8c6b074e5c8669b808caf6726f102fc7babfa9
Yoann Cribier <yoann_cribier@hotmail.com>
2018-05-28 13:42:55 +0200

    mongo - mapIds helpers added (Replace `_id` (ObjectId) field by an `id` (String) field)

--- src/db/mongodb.js
+++ src/db/mongodb.js
@@ -3,6 +3,21 @@ const url = process.env.DATABASE_URL || 'mongodb://localhost:27017/livecodings'
 
 const db = connect(url)
 
+// Helpers
+
+// Replace `_id` (ObjectId) field by an `id` (String) field
+const mapId = r => {
+  const { _id, ...rest } = r
+
+  return { id: _id.toString(), ...rest }
+}
+const mapIds = r => {
+  if (!r) return
+
+  return Array.isArray(r) ? r.map(mapId) : mapId(r)
+}
+const toId = f => (...args) => f(...args).then(mapIds)
+
 // Collections
 
 const users = db.get('users')
@@ -13,27 +28,27 @@ const todos = db.get('todos')
 
 // USERS
 
-const readUsers = () => users.find({})
-readUsers.byId = id => users.findOne({ _id: id })
-readUsers.byEmail = email => users.findOne({ email })
+const readUsers = () => users.find({}).then(mapIds)
+readUsers.byId = id => users.findOne({ _id: id }).then(mapIds)
+readUsers.byEmail = email => users.findOne({ email }).then(mapIds)
 
-const createUser = user => users.insert({ ...user, createdAt: new Date() })
+const createUser = user => users.insert({ ...user, createdAt: new Date() }).then(mapIds)
 
-const updateUser = update => users.findOneAndUpdate({ _id: update.id }, { $set: update })
+const updateUser = update => users.findOneAndUpdate({ _id: update.id }, { $set: update }).then(mapIds)
 
-const deleteUser = id => users.findOneAndDelete({ _id: id })
+const deleteUser = id => users.findOneAndDelete({ _id: id }).then(mapIds)
 
 
 // TODOS
 
-const readTodos = () => todos.find({})
-readTodos.byId = id => todos.findOne({ _id: id })
+const readTodos = () => todos.find({}).then(mapIds)
+readTodos.byId = id => todos.findOne({ _id: id }).then(mapIds)
 
-const createTodo = todo => todos.insert({ ...todo, stars: [], createdAt: new Date() })
+const createTodo = todo => todos.insert({ ...todo, stars: [], createdAt: new Date() }).then(mapIds)
 
-const updateTodo = update => todos.findOneAndUpdate({ _id: update.id }, { $set: update })
+const updateTodo = update => todos.findOneAndUpdate({ _id: update.id }, { $set: update }).then(mapIds)
 
-const deleteTodo = id => todos.findOneAndDelete({ _id: id })
+const deleteTodo = id => todos.findOneAndDelete({ _id: id }).then(mapIds)
 
 const vote = async ({ todoId, userId }) => {
   const todo = await readTodos.byId(todoId)
